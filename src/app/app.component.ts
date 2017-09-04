324c9e70a6feda0ee51eb43770dabce56f5f0a08
Angular CLI <angular-cli@angular.io>
2017-09-04 15:40:41 +0300

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'msbit-core-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'msbit-core';
+}
